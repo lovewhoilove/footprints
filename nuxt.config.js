85e9c5c18f8e7e826c249e10b81d2eeecceb888c
lovewhoilove <3528300287@qq.com>
2021-12-28 10:36:06 +0800

    借助koa开发注册、查询用户的接口

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,49 +1,53 @@
-export default {
+module.exports = {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  /*
+   ** Headers of the page
+   */
   head: {
-    title: 'footprints',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'element-ui/lib/theme-chalk/index.css',
-    '~/assets/common.css',
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+  /*
+   ** Global CSS
+   */
+  css: ["element-ui/lib/theme-chalk/index.css", "~/assets/common.css"],
+  /*
+   ** Plugins to load before mounting the App
+   */
   plugins: [
-    '@/plugins/element-ui',
-    { src: "@/plugins/vue-leaflet.js", ssr: false },
+    "@/plugins/element-ui",
+    { src: "@/plugins/vue-leaflet.js", ssr: false }
   ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+  /*
+   ** Build configuration
+   */
   build: {
     transpile: [/^element-ui/],
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) { }
   }
-}
+};
